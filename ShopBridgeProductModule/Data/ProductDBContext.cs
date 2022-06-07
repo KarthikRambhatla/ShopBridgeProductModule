f9f0aba331ede00c820977336b0139f6f6d83e58
Karthik Rambhatla <karthikrambhatla@gmail.com>
2022-06-07 12:13:57 +0530

    Add project files.

---
+++ ShopBridgeProductModule/Data/ProductDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopBridgeProductModule.Models;
+
+namespace ShopBridgeProductModule.Data
+{
+    public class ProductDBContext:DbContext
+    {
+        public ProductDBContext(DbContextOptions<ProductDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; } = null!;
+    }
+}
